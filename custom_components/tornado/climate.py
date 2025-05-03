97acb70a5bf3f9b4d05cf9b5b0ff6bc7aed7c040
JerryVDP <99589825+JerryVDP@users.noreply.github.com>
2025-05-03 15:12:12 +0300

    Fix modes

--- custom_components/tornado/climate.py
+++ custom_components/tornado/climate.py
@@ -31,11 +31,11 @@ _LOGGER = logging.getLogger(__name__)
 
 # Map Tornado modes to Home Assistant modes
 HVAC_MODE_MAP = {
-    0: HVACMode.AUTO,
+    0: HVACMode.COOL,
     1: HVACMode.HEAT,
-    2: HVACMode.COOL,
+    2: HVACMode.DRY,
     3: HVACMode.FAN_ONLY,
-    4: HVACMode.DRY,
+    4: HVACMode.AUTO,
 }
 
 HVAC_MODE_MAP_REVERSE = {v: k for k, v in HVAC_MODE_MAP.items()}
